4ac9b932010a7287baa0fe74087f5622712b4e0e
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2022-05-21 17:54:32 -0700

    flip

--- Texture.cpp
+++ Texture.cpp
@@ -32,6 +32,7 @@ namespace GayCubes
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		stbi_set_flip_vertically_on_load(true);
 		// load texture
 		int w, h, nrChannels;
 		unsigned char* data = stbi_load(_textureFilePath, &w, &h, &nrChannels, 0);
